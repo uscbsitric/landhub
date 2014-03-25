3315ad9a9bc24c793824251906e778b68a4ad002
uscbsitric <usc_bsit_ric@yahoo.com>
2014-03-25 17:06:46 +0800

    added the following: Controller: Twilio Template: Twilio
    database dumps:
    citiesAndListings.sql
    data_synd_platform.sql

---
+++ www/application/classes/Controller/Twilio.php
@@ -0,0 +1,11 @@
+<?php
+defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Twilio extends Controller_Template
+{
+	public $template = 'templates/twilio';
+	
+	public function action_craigslistland()
+	{
+	}
+}
---
+++ www/application/views/templates/twilio.php
@@ -0,0 +1,9 @@
+<?php
+	echo header("content-type: text/xml");
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	echo "
+	<Response>
+	<Say>Hello Monkey this is Frederick Sandalo of Ripe Concepts working for Landhub with Jeran and John</Say>
+	</Response>";
+	//echo @$content; 
+?>
