8506ef07e60379749a53eb87df9b433a9c958ffa
uscbsitric <usc_bsit_ric@yahoo.com>
2014-05-22 16:57:10 +0800

    frederick removing the debugging and applied the neccessary changes to IMAPHandler.php

--- public_html/modules/lsp/classes/Model/IMAPHandler.php
+++ public_html/modules/lsp/classes/Model/IMAPHandler.php
@@ -93,31 +93,15 @@ class Model_IMAPHandler extends Model
 
 				foreach($emails as $email_number)
 				{
-					/* get information specific to this email */
-					$overview = imap_fetch_overview($this->inbox,$email_number,0);
-					$message  = imap_fetchbody($this->inbox,$email_number,2);
+					$emailMessage  = imap_fetchbody($this->inbox,$email_number,2);
+					preg_match_all('/[\/u\/][\w-]+/', $emailMessage, $result);
+					$craigslistPostingURL = 'https://post.craigslist.org/u' . $result[0][8] . $result[0][9];
 
-					/* output the email header information */
-					$output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-					$output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-					$output.= '<span class="from">'.$overview[0]->from.'</span>';
-					$output.= '<span class="date">on '.$overview[0]->date.'</span>';
-					$output.= '</div>';
-
-					/* output the email body */
-					$output.= '<div class="body">'.$message.'</div>';
+					break;
 				}
 
-				echo $output;
+				return $craigslistPostingURL;
 			}
-
-			exit('frederick debugging here');
-
-			$emailMessage = imap_fetchbody($this->inbox, 1, 2);  // mo work ra if mo perform ko og imap_open() first, i think.... lets see
-			preg_match_all('/[\/u\/][\w-]+/', $emailMessage, $result);
-			$craigslistPostingURL = 'https://post.craigslist.org/u' . $result[0][8] . $result[0][9];
-
-			return $craigslistPostingURL;
 		}
 
 		return false;
