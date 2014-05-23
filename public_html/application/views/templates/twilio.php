1b176e8dabd855928d1dcd440948520b8faaa388
uscbsitric <usc_bsit_ric@yahoo.com>
2014-05-23 11:31:59 +0800

    updated public_html/application/views/templates/twilio.php to reflect the changes to where Twilio will be requesting to

--- public_html/application/views/templates/twilio.php
+++ public_html/application/views/templates/twilio.php
@@ -2,7 +2,7 @@
 	echo header("content-type: text/xml");
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	echo "<Response>
-		  	<Record transcribe='true' transcribeCallback='http://staging.landhub.com/twilio/parsetwilio' />
+		  	<Record transcribe='true' transcribeCallback='http://mynewsite.landhub.webfactional.com/twilio/parsetwilio' />
 		  </Response>
 		 ";
 	//echo @$content; 
